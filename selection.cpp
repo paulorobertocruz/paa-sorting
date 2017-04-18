174b9d781cdce51e9c0a31f5b3ebf0e1d57665ea
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2017-04-18 11:14:30 -0300

    atuliza selection sort

---
+++ selection.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+vector<int> v;
+int trocas = 0;
+int comparacoes = 0;
+
+void selectoin_sort()
+{    
+    
+    int size;
+
+    int i, j;
+    int maxPosition = 0;
+    int temp = 0;
+    
+    size = v.size();
+
+    for(i = size-1; i >= 0; i--)
+    {
+
+        maxPosition = 0;
+
+        for(j = 1; j < i + 1; j++)
+        {
+            comparacoes += 1;
+            if( v[j] > v[maxPosition] )
+            {
+                maxPosition = j;
+            }
+        }
+
+        comparacoes += 1;
+        if (i != maxPosition)
+        {
+            //troca
+            trocas += 1;
+            temp = v[i];
+            v[i] = v[maxPosition];
+            v[maxPosition] = temp;
+        }
+        
+
+    }
+
+}
+
+
+void leituraChamada(string entrada, string saida)
+{
+  ifstream leitura(entrada.c_str()); // tipo para leitura de arquivo
+  ofstream escrita(saida.c_str()); // tipo para escrita de arquivo
+  clock_t inicio, fim;       // para calcular tempo de cpu
+  double tempo;
+  int aux;
+
+  //Leitura do arquivo ----------------------------------------
+  while(leitura >> aux)
+    v.push_back(aux); // escrita no vector
+  //-----------------------------------------------------------
+
+  inicio = clock(); // início da contagem do tempo de CPU
+  selectoin_sort(); // chamada do método de ordenação
+  fim = clock(); // Fim da contagem do tempo de CPU
+
+  tempo = (fim - inicio)/(double)CLOCKS_PER_SEC; //Cálculo do tempo de CPU
+
+  //Escrita no arquivo ---------------------------------------
+  escrita << "Tempo: " << tempo << "\n";
+  //escrita << "Trocas: " << trocas << "\n";
+  //escrita << "Comparacoes" << comparacoes << "\n";
+
+  for(int i = 0; i < v.size(); i++)
+  escrita << v[i] << " ";//escrita dos dados ordenados arquivo de saída
+  //----------------------------------------------------------
+}
+
+
+int main(int argc, char **argv)
+{   
+    leituraChamada("./entradas/desordenados/ODE-100.txt", "./saidas/selection/desordenadas/SO-100.txt");
+    leituraChamada("./entradas/desordenados/ODE-1000.txt", "./saidas/selection/desordenadas/SO-1000.txt");
+    leituraChamada("./entradas/desordenados/ODE-10000.txt", "./saidas/selection/desordenadas/SO-10000.txt");
+    leituraChamada("./entradas/desordenados/ODE-50000.txt", "./saidas/selection/desordenadas/SO-50000.txt");
+    leituraChamada("./entradas/desordenados/ODE-100000.txt", "./saidas/selection/desordenadas/SO-100000.txt");
+
+}
